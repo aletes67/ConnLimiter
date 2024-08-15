497787f3fc341d2c48ce1dba6dc56fd655b49ce2
Alessandro Testa <contatto@alessandrotesta.info>
2024-08-15 18:43:20 +0200

    aggiunta provider e mail abuse nel file di log

--- banna.sh
+++ banna.sh
@@ -13,26 +13,35 @@ num_ore=$2
 # Rimuovi il .0 finale per controllare contro la lista di IP da ignorare
 classe_ip_no_dot_zero=$(echo $classe_ip | sed 's/\.0\/24//')
 
-# Verifica se l'IP senza .0 \Uffffffffella lista di esclusione
+# Verifica se l'IP senza .0 è nella lista di esclusione
 if grep -q "^$classe_ip_no_dot_zero" /etc/ddos/ignore.ip.list; then
-    echo "L'IP $classe_ip_no_dot_zero  e in lista di esclusione. Nessuna azione eseguita."
+    echo "L'IP $classe_ip_no_dot_zero in lista di esclusione. Nessuna a
+zione eseguita."
     exit 0
 fi
 
+# Ottieni le informazioni del provider e l'email abuse usando whois
+whois_info=$(whois $classe_ip)
+provider_name=$(echo "$whois_info" | grep -i 'OrgName\|netname\|descr' | head -n
+ 1 | awk -F: '{print $2}' | xargs)
+abuse_email=$(echo "$whois_info" | grep -i 'abuse\|abuse-mailbox' | head -n 1 | 
+awk -F: '{print $2}' | xargs)
+
 # Esegui il blocco dell'IP con iptables
 sudo iptables -I INPUT 1 -s $classe_ip -j DROP
-echo "Blocco della classe IP $classe_ip eseguito con successo."
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Blocco della classe IP $classe_ip eseguito 
+con successo. Provider: $provider_name, Email abuse: $abuse_email."
 
-# Calcola l'orario in cui il blocco sar\Uffffffffimosso
+# Calcola l'orario in cui il blocco sara rimosso
 unban_time=$(date -d "+$num_ore hours" +"%Y-%m-%d %H:%M:%S")
 
 # Scrivi l'IP bannato e l'ora di rimozione nel file di log
 log_file="./ip_ban.log"
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Classe IP $classe_ip bannata. Rimozione prevista per: $unban_time" | sudo tee -a $log_file
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Classe IP $classe_ip bannata. Rimozione pre
+vista per: $unban_time. Provider: $provider_name, Email abuse: $abuse_email" | s
+udo tee -a $log_file
 
 # Pianifica la rimozione del blocco con at
 echo "sudo iptables -D INPUT -s $classe_ip -j DROP" | at now + $num_ore hours
 echo "Il blocco sara rimosso automaticamente tra $num_ore ore."
-
-
 
--- unbannaip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for filename in /home/ubuntu/unban/*.txt; do
-    for ((i=0; i<=3; i++)); do
-        echo "$filename" 
-    done
-done
-
