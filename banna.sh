d6c1680370e35fdbee39a9a858592cdec6e9adf8
Alessandro Testa <contatto@alessandrotesta.info>
2024-08-20 21:34:02 +0200

    fix whois

--- banna.sh
+++ banna.sh
@@ -14,35 +14,50 @@ num_ore=$2
 classe_ip_no_dot_zero=$(echo $classe_ip | sed 's/\.0\/24//')
 classe_ip_no_24=$(echo $classe_ip | sed 's/\/24//')
 
-# Verifica se l'IP senza .0 e nella lista di esclusione
+# Verifica se l'IP senza .0 nella lista di esclusione
 if grep -q "^$classe_ip_no_dot_zero" /etc/ddos/ignore.ip.list; then
     echo "L'IP $classe_ip_no_dot_zero e in lista di esclusione. Nessuna azione eseguita."
     exit 0
 fi
 
-# Ottieni le informazioni del provider e l'email abuse usando whois
-whois_info=$(whois $classe_ip_no_24)
-provider_name=$(echo "$whois_info" | grep -i 'OrgName:\|netname:' | head -n 1 | awk -F: '{print $2}' | xargs)
 
-# Estrai l'email di contatto abuse (sia "Abuse contact" sia "OrgAbuseEmail")
-abuse_email=$(echo "$whois_info" | grep -iE "(Abuse contact for|OrgAbuseEmail:)" | awk '{print $NF}')
+# Funzione per estrarre il netname, organization e l'email di abuse
+extract_info() {
+    local whois_output="$1"
 
-# Se l'email non e stata trovata con le espressioni precedenti, controlla altre linee con "abuse"
-if [ -z "$abuse_email" ]; then
-    abuse_email=$(echo "$whois_info" | grep -i "abuse" | grep -Eo "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}")
-fi
+    # Estrai l'organizzazione, se disponibile
+    local organization=$(echo "$whois_output" | grep -iE "(organization:|CustName:)" | awk -F': ' '{print $2}' | head -n 1)
+
+    # Se l'organizzazione non disponibile, estrai il netname
+    if [ -z "$organization" ]; then
+        organization=$(echo "$whois_output" | grep -iE "(netname:)" | awk -F': ' '{print $2}' | head -n 1)
+    fi
+
+    # Estrai le email di contatto abuse (sia "Abuse contact" sia "OrgAbuseEmail" sia linee con "abuse")
+    local abuse_email=$(echo "$whois_output" | grep -iE "(Abuse contact|OrgAbuseEmail:|abuse)" | grep -Eo "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}")
+
+    echo "$organization" "$abuse_email"
+}
+
+# Esegui il comando whois sull'IP
+whois_output=$(whois $ip_classei_no_24)
+
+# Estrai le informazioni
+read organization abuse_email <<< $(extract_info "$whois_output")
+
+
+
 
 # Esegui il blocco dell'IP con iptables
 sudo iptables -I INPUT 1 -s $classe_ip -j DROP
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Blocco della classe IP $classe_ip eseguito con successo. Provider: $provider_name, Email abuse: $abuse_email."
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Blocco della classe IP $classe_ip eseguito con successo. Provider: $organization, Email abuse: $abuse_email."
 
 # Calcola l'orario in cui il blocco sara rimosso
 unban_time=$(date -d "+$num_ore hours" +"%Y-%m-%d %H:%M:%S")
 
 # Scrivi l'IP bannato e l'ora di rimozione nel file di log
 log_file="./ip_ban.log"
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Classe IP $classe_ip bannata. Rimozione prevista per: $unban_time. Provider: $provider_name, Email abuse: $abuse_em
-ail" | sudo tee -a $log_file
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Classe IP $classe_ip bannata. Rimozione prevista per: $unban_time. Provider: $organization, Email abuse: $abuse_email" | sudo tee -a $log_file
 
 # Pianifica la rimozione del blocco con at
 echo "sudo iptables -D INPUT -s $classe_ip -j DROP" | at now + $num_ore hours
